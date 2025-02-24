ed393eb35fc4f986b05478efd83f5910bebe09e7
Chris Roughsedge <chris.roughsedge@moneypenny.co.uk>
2025-02-24 15:52:10 +0000

    no message

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,24 +1,23 @@
 <template>
   <div id="container" :style="{ backgroundImage: 'url(' + require('../assets/pitch.jpg') + ')'}">
     <div class="jumbotron" style="background-color: transparent;padding:40px;margin: 0;">
-      <h1 class="display-4" style="padding-top: 50px;">Match analysis</h1>
+      <h1 class="display-4" style="padding-top: 50px;">the match analyst</h1>
       <hr class="my-4">
       <p class="lead" style="padding-bottom: 50px;">
-        Football and match analysis comprises of lots of different areas of analysis. I have been conducting analysis in all forms over the last 3 years, more 
-        prominantly in data and opposition analysis.
+        I am a qualified football tactical analyst working in football analysis for the past 3 and a half years. Learning my trade with FC Barcelona 
+        and working with Spanish youth teams and 'B' sides,  I now work in the lower levels of English football providing football analysis to several different clubs.
       </p>
     </div>
     
       <div class="jumbotron" style="background-color: #F2F2F2; color: #38404B">
         <div class="row">
             <div class="col-sm-4" style="padding:50px">
-              <h1 class="display-4">Data analysis</h1>
+              <h1 class="display-4">Follow me</h1>
             </div>
             <div class="col-sm-6" style="padding:50px">
-                Data is captured in lots of different ways from different football platforms. GPS data is now captured along with event data. I have been 
-                working primarily with StatsBomb data but have also used Wyscout data and GPS data. The issue with data commonly available at the moment 
-                is that there is no context so a pass completed doesn't mean a lot without the context. This is where combining GPS and event data comes in handy. Not just
-                that but using my own custom build data capture tools I can also capture the context of any given event without access to that platform owned data.
+                I will be posting new videos on YouTube as often as I can with the primary focus being tactical analysis in the lower leagues of English football. 
+                The lower leagues are sadly not well represented in the media so I plan to bring focus to those leagues offering the type of analysis I over to the 
+                clubs that I report for.
             </div>
         </div>  
         <p class="lead">
@@ -28,30 +27,48 @@
       <div class="jumbotron">
       <div class="row">
           <div class="col-sm-6" style="padding: 50px;">
-              Opposition and team analysis is increasingly more important in todays game. The ability to scrutinise opposition teams providing strengths and
+              Opposition and team analysis is increasingly more important in todays game. The ability to scrutinise opposition teams, providing strengths and
               weaknesses to the coaching staff can be the difference between winning and losing. I have been providing reports over the last 3 years for clubs 
-              in the MLS, EFL2, EFL1 and more recently in the National League. 
+              in the MLS, EFL2, EFL1, National League and National league north and south. 
           </div>
           <div class="col-sm-4">
             <h1 class="display-4" style="padding:50px">Opposition and team analysis</h1>
           </div>
       </div>  
-      <p class="lead">
-      </p>
-    </div>
+
+      
+        <p class="lead">
+        </p>
+      </div>
 
 
-    <div class="jumbotron" style="background-color: #F2F2F2; color: #38404B;margin-bottom: 0px;padding-bottom:10px">
+    <div class="jumbotron" style="background-color: #F2F2F2; color: #38404B;">
         <div class="row">
             <div class="col-sm-4" style="padding:50px">
-              <h1 class="display-4">Contact us</h1>
+              <h1 class="display-4">Data analysis</h1>
             </div>
             <div class="col-sm-6" style="padding:50px">
-              If you would like to contact us and talk about our analysis to see what we can offer then please feel free to email us at
-              any time and we will get straight back to you.<br />
+              Data has become more and more important in todays game. I have previously worked with event data from StatsBomb and Wyscout to provide 
+              a deeper look into a teams data and more meaningful insight that can be used to gain a competitive advantage. 
+            </div>
+        </div>  
+        <p class="lead">
+        </p>
+      </div>
+
+            <div class="jumbotron" style="margin-bottom: 0px;padding-bottom:10px">
+      <div class="row">
+            <div class="col-sm-4" style="padding:50px;">
+              <h1 class="display-4">Contact me</h1>
+            </div>
+            <div class="col-sm-6" style="padding:50px">
+              If you would like to contact me and talk about the analysis I do then please feel free to email me at
+              any time and I will get straight back to you.<br />
               Email: <a href="mailto:chrisjr.analyst@gmail.com">chrisjr.analyst@gmail.com</a>
             </div>
         </div>  
+
+      
         <p class="lead">
         </p>
       </div>
--- src/views/SiteNavigation.vue
+++ src/views/SiteNavigation.vue
@@ -2,12 +2,12 @@
 <div class="container-fluid">
   <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #38404B;padding: 20px;margin:0px">
   <div class="container-fluid">
-    <router-link to="/" class="navbar-brand" style="color:#F2F2F2">Match analysis</router-link>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <router-link to="/" class="navbar-brand" style="color:#F2F2F2">the match analyst</router-link>
+    <!--<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
-    </button>
+    </button>-->
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <!--<ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
           <router-link to="/data-analysis" class="nav-link" style="color:#F2F2F2">Data Analysis</router-link>
         </li>
@@ -20,7 +20,7 @@
         <li class="nav-item">
           <router-link to="/aboutme" class="nav-link" style="color:#F2F2F2">About me</router-link>
         </li>
-      </ul>
+      </ul>-->
       
     </div>
   </div>
